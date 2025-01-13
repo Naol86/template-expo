6c729f2b4acefa25a79bb36577fa3c06af100d30
naol86 <naol.kasinet@a2sv.org>
2025-01-13 22:49:13 +0300

    starter

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,5 +3,13 @@ import { Stack } from "expo-router";
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="modal"
+        options={{ headerShown: false, presentation: "modal" }}
+      />
+    </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
     <View className="items-center justify-center grow">
       <Text className="text-3xl text-red-600">Index</Text>
+      <Link href="(tabs)/home">go to tabs</Link>
+      <Link href="modal">open modal</Link>
     </View>
   );
 }
