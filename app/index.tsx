6fe55a282bcb30703dd0cfb6b79193e64771e9b6
naol86 <naol.kasinet@a2sv.org>
2025-01-13 22:22:02 +0300

    init

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function Index() {
+  return (
+    <View className="items-center justify-center grow">
+      <Text className="text-3xl text-red-600">Index</Text>
+    </View>
+  );
+}
